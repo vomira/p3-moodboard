ec79b7848915fc45d411f50b768c9beec02e1f1b
Mira Vogt <miravogt@gmail.com>
2020-10-08 17:20:33 +0200

    commit build

---
+++ client/build/precache-manifest.2b641898b2476092131914d5e7e8fe88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd41cf55c6fc5f1f5e57aaa49c872181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b674c7945a05bacdf5",
+    "url": "/static/css/2.78afd188.chunk.css"
+  },
+  {
+    "revision": "6f7416f6a9a653da76e0",
+    "url": "/static/css/main.077f132b.chunk.css"
+  },
+  {
+    "revision": "08b674c7945a05bacdf5",
+    "url": "/static/js/2.473d86f6.chunk.js"
+  },
+  {
+    "revision": "1849f4a3e5cd5398e09345378938d1bc",
+    "url": "/static/js/2.473d86f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7416f6a9a653da76e0",
+    "url": "/static/js/main.2943b2c6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "9e4cc1b3c3de425839c25289558b53f8",
+    "url": "/static/media/logo_trans_cropped.9e4cc1b3.png"
+  }
+]);
