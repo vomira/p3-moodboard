c8e50b9d5ec66d7354422cd3a8c046f09f2b7477
Mira Vogt <miravogt@gmail.com>
2020-10-08 22:18:16 +0200

    remove favorite from newscard

--- client/build/precache-manifest.9fe7577a29d4669f287bda6c356f54e2.js
+++ client/build/precache-manifest.9fe7577a29d4669f287bda6c356f54e2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aacbe66e191d875d25532bad1795b830",
+    "revision": "b6948932d1387066a87f57e6b9965627",
     "url": "/index.html"
   },
   {
-    "revision": "9403feee43d5b26f94a8",
+    "revision": "b0a20dd390478824c13a",
     "url": "/static/css/2.78afd188.chunk.css"
   },
   {
-    "revision": "408d7bdabd6cb77ffa6f",
+    "revision": "81e75ae47f48aaa1937e",
     "url": "/static/css/main.86bce7c7.chunk.css"
   },
   {
-    "revision": "9403feee43d5b26f94a8",
-    "url": "/static/js/2.815652b5.chunk.js"
+    "revision": "b0a20dd390478824c13a",
+    "url": "/static/js/2.8bc9eb56.chunk.js"
   },
   {
     "revision": "1849f4a3e5cd5398e09345378938d1bc",
-    "url": "/static/js/2.815652b5.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8bc9eb56.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "408d7bdabd6cb77ffa6f",
-    "url": "/static/js/main.b23b9ccc.chunk.js"
+    "revision": "81e75ae47f48aaa1937e",
+    "url": "/static/js/main.fef9ac4c.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
